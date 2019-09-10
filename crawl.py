9089b9a3b1c10a5d49f6ae2d2f7c5f5121466390
Casey Dwyer <caseydwyer@gmail.com>
2019-09-10 10:54:33 -0700

    Update.

--- crawl.py
+++ crawl.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
+
 import csv
 import datetime
 import html
 import json
 import os
 import subprocess
-import sys
 import urllib.request
-
-from bs4 import BeautifulSoup
 
 start = datetime.datetime.now()
 
 SERIES = ['motogp', 'moto2', 'moto3']
 MAX_SEASONS = 20
-IGNORE_KEYS = {'type', 'code', 'uuid', 'picture', 'hasResults', 'preciseStartTime'}
+
+IGNORE_KEYS = {'type', 'code', 'uuid', 'picture',
+               'race', 'winner', 'venue',
+               'hasResults', 'preciseStartTime'}
+
 # DATE_KEYS = {'startTimeUtc', 'endTimeUtc'}
 DATE_KEYS = {'date', 'startTime', 'endTime'}
 CACHE_DIR = 'cache'
@@ -22,7 +24,6 @@ CACHE_DIR = 'cache'
 API_BASE = 'https://mssproxy.motorsportstats.com/web/3.0.0/'
 SEASONS_URL =   API_BASE + 'series/%s/seasons'
 RACES_URL =     API_BASE + 'seasons/%s/races'
-CONST_URL =     API_BASE + 'seasons/%s/standings/constructors'
 SESSIONS_URL =  API_BASE + 'events/%s/sessions'
 CLASS_URL =     API_BASE + 'sessions/%s/classification'
 
@@ -32,17 +33,8 @@ stats = {
     'events': 0,
     'sessions': 0,
     'pages scraped': 0,
-    'rows of data': 0,
+    'classifications': 0,
 }
-
-
-def human_size(n):
-    SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB']
-    i = 0
-    while n > 1000:
-        i += 1
-        n /= 1000
-    return '{0:.02f} {1}'.format(n, SIZE_UNITS[i])
 
 
 class Opener(urllib.request.URLopener):
@@ -51,11 +43,7 @@ class Opener(urllib.request.URLopener):
         stats['pages scraped'] += 1
         filename = os.path.join(CACHE_DIR, url.lstrip('https://'))
         if not os.path.exists(filename):
-            dirname, basename = os.path.split(filename)
-            try:
-                os.makedirs(dirname)
-            except:
-                pass
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
             print('getting', url)
             self.retrieve(url, filename)
         with open(filename) as fp:
@@ -66,7 +54,7 @@ class Opener(urllib.request.URLopener):
         return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
 
 
-def flatten(src, dst, prefix=''):
+def flatten(dst, src, prefix=''):
     for key, value in src.items():
         if key in IGNORE_KEYS:
             continue
@@ -74,12 +62,13 @@ def flatten(src, dst, prefix=''):
             assert len(value) == 1
             value = value[0]
         if isinstance(value, dict):
-            flatten(value, dst, prefix + key + '_')
+            flatten(dst, value, prefix + key + '_')
         else:
             if value and key in DATE_KEYS:
                 value = datetime.datetime.fromtimestamp(value)
             dst[prefix + key] = value
             fieldnames[prefix + key] = 1
+
 
 def getseries(series):
     opener = Opener()
@@ -89,33 +78,25 @@ def getseries(series):
     seasons_url = SEASONS_URL % series
     for season in opener.get(seasons_url)[:MAX_SEASONS]:
         stats['seasons'] += 1
-        season_id = season['season']['uuid']
-        races_url = RACES_URL % season_id
-        races = opener.get(races_url)
-        for race in races:
+        for race in opener.get(RACES_URL % season['season']['uuid']):
             stats['events'] += 1
-            del race['race']
-            del race['winner']
-            del race['venue']
-            race_id = race['event']['uuid']
-            for session in opener.get(SESSIONS_URL % race_id):
+            for session in opener.get(SESSIONS_URL % race['event']['uuid']):
                 if not session['hasResults']:
                     continue
                 stats['sessions'] += 1
-                session_id = session['session']['uuid']
-                classifications = opener.get(CLASS_URL % session_id)
+                classifications = opener.get(CLASS_URL % session['session']['uuid'])
                 for classification in classifications['details']:
                     row = {}
-                    flatten(season, row)
-                    flatten(race, row)
-                    flatten(session, row)
-                    flatten(classification, row)
+                    flatten(row, season)
+                    flatten(row, race)
+                    flatten(row, session)
+                    flatten(row, classification)
                     rows.append(row)
     with open(filename, 'w') as fp:
-        writer = csv.DictWriter(fp, fieldnames.keys())
+        writer = csv.DictWriter(fp, fieldnames)
         writer.writeheader()
         writer.writerows(rows)
-        stats['rows of data'] += len(rows)
+        stats['classifications'] += len(rows)
     print(fieldnames.keys())
     return filename
 
@@ -130,18 +111,27 @@ def combine_csv(srcs, dst):
                     writer.writerow(row)
 
 
+def sizestr(n):
+    SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB']
+    i = 0
+    while n >= 1000:
+        i += 1
+        n /= 1000
+    return '{0:.02f} {1}'.format(n, SIZE_UNITS[i])
+
+
 if __name__ == '__main__':
     fieldnames = {}
     files = []
     for series in SERIES:
         files.append(getseries(series))
-    combined_csv = 'all.csv'
+    combined_csv = 'allseries.csv'
     combine_csv(files, combined_csv)
     files.append(combined_csv)
-    cache_tar = CACHE_DIR + '.tar.gz'
-    subprocess.call(['tar', 'czf', cache_tar, CACHE_DIR])
-    files.append(cache_tar)
-    files.append(os.path.basename(__file__))
+    # cache_tar = CACHE_DIR + '.tar.gz'
+    # subprocess.call(['tar', 'czf', cache_tar, CACHE_DIR])
+    # files.append(cache_tar)
+    # files.append(os.path.basename(__file__))
     now = datetime.datetime.now()
     with open('index.html', 'w') as fp:
         fp.write('<html><head><title>moto data</title></head><body>')
@@ -151,12 +141,15 @@ if __name__ == '__main__':
         fp.write('<ul>')
         for filename in files:
             fp.write('<li><a href="{0}">{0}</a> ({1})</li>'.format(
-                filename, human_size(os.path.getsize(filename))))
+                filename, sizestr(os.path.getsize(filename))))
         fp.write('</ul>')
         fp.write('<h2>Stats</h2>')
         fp.write('<ul>')
         for stat in stats.items():
             fp.write('<li>{1} {0}</li>'.format(*stat))
         fp.write('</ul>')
+        # fp.write('<h2>Code</h2>')
+        # with open(__file__) as gp:
+        #     fp.write('<pre>%s</pre>' % html.escape(gp.read()))
         fp.write('<p>Generated in %s</p>' % (now-start))
         fp.write('</body></html>')
